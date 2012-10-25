ac13b4d9abe8b839d633779a968dfc3f1ff06374
Tim Branyen <tim@tabdeveloper.com>
2012-10-24 23:25:58 -0400

    initial commit

---
+++ backbone.cacheit.js
@@ -0,0 +1,62 @@
+/*!
+* backbone.cacheit.js v0.1.0
+* Copyright 2012, Tim Branyen (@tbranyen)
+* backbone.cacheit.js may be freely distributed under the MIT license.
+*/
+(function(window) {
+
+"use strict";
+
+// Localize global dependency references.
+var Backbone = window.Backbone;
+var $ = window.$;
+var _ = window._;
+
+// Retain a copy of the original fetch method, since we are overidding it.
+var fetch = Backbone.Collection.prototype.fetch;
+
+// Patch the fetch method to retain a reference.
+_.each(["Model", "Collection"], function(ctor) {
+  // Override both Model and Collection `fetch` methods.
+  var newFetch = Backbone[ctor].prototype.fetch = function(options) {
+    options = options || {};
+
+    // Save a reference to the original deferred.
+    var old = this._def;
+
+    // If a deferred doesn't exist, create one.  If the clear flag is provided,
+    // jump in to create a new deferred.
+    if (!this._def || options.reload || this.reload) {
+      // 
+      this._def = newFetch.deferred();
+
+      // If the clear was provided and there is an existing deferred, resolve it
+      // once this has resolved.
+      if (options.reload && old) {
+        this._def.done(old.resolve);
+      }
+    } else {
+      // Return early.
+      return this._def;
+    }
+
+    // Call the original `fetch` method and store its return value (jqXHR).
+    var req = fetch.apply(this, arguments);
+
+    // Once the request has finished, resolve this deferred.
+    req.done(_.bind(function() {
+      this._def.resolveWith(this, [this]);
+    }, this));
+
+    // Return the deferred to wait with.
+    return this._def;
+  };
+
+  // Allow the jQuery dependency to be swapped out to use this in other
+  // enviornments.
+  Backbone[ctor].prototype.fetch.deferred = function() {
+    return $.Deferred();
+  };
+});
+
+})(this);
---
+++ readme.md
@@ -0,0 +1,49 @@
+backbone.cacheit v0.0.1
+=======================
+
+Created by Tim Branyen [@tbranyen](http://twitter.com/tbranyen).
+
+This is useful for when you have Models and Collections that once data is
+fetched you can operate with the same dataset throughout the lifetime of your
+application.  Whatever route your application starts with can safely call the
+`fetch` method to get the data into the instances.
+
+Tested with Underscore, Backbone and jQuery. You can swap out jQuery with a
+custom configuration or substitute Underscore with Lo-Dash.
+
+## Documentation ##
+
+Every `fetch` method is patched to return a Deferred, instead of the `jqXHR`
+object in `jQuery.ajax`.  If you call `fetch` multiple times while it is
+fetching or after it's fetched, you will always get the same Deferred and it
+will not make additional requests.
+
+Cancelling is super simple, just add the `reload` option:
+
+``` javascript
+fetch({ reload: true });
+```
+
+optionally you can make a Model or Collection always `reload` by doing the
+following (works with Models too):
+
+``` javascript
+Backbone.Collection.extend({
+  reload: true
+}):
+```
+
+If you want use `underscore.deferred` or some other implementation you can
+specify an override to swap out that dependency.
+
+``` javascript
+Backbone.Collection.prototype.fetch.deferred = function() {
+  return _.Deferred();
+}
+```
+
+## Release notes ##
+
+### 0.0.1 ###
+
+* Initial beta release.
